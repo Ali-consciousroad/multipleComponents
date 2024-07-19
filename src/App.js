c03c198dcbb15027a7a4b86c97c71b637c4b0f64
ali-consciousroad <ali_dindar@live.be>
2024-07-19 16:37:40 +0200

    Add comments for better readability

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div>
       <h1>Task: Add three Card elements</h1>
+      {/* Create multiple copies of the <Card/> component */}
       <Card h2="First card's h2" />
       <Card h3="Second card's h3"/>
       <Card h2="Third card's h2" h3="Third card's h3"/>
